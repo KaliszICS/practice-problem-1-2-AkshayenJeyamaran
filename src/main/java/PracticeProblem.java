ad1729ff9a4d55426c39fc7b0d5dabd79edfad31
AkshayenJeyamaran <akshayen.jeyamaran@student.tdsb.on.ca>
2026-03-03 15:07:39 +0000

    finish all questions

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,30 @@ public class PracticeProblem {
 
 	}
 
-	
+	public static boolean validIndex (int[] numbers, int index)  {
+		try {
+			int int1;
+			int1 = numbers[index];
+			return true;
+		} catch (Exception e) {
+        	return false; 
+		}
+	}
+	public static int divide(int int1, int int2) {
+    try {
+        int int3 = int1 / int2;
+        return int3;
+    } catch (Exception e) {
+        return 0;
+    }
+	}
 
+	public static int safeConvertStringtoInt (String s) {
+    try {
+        int int3 = Integer.parseInt(s);
+        return int3;
+    } catch (Exception e) {
+        return 0;
+    }
+	}
 }
